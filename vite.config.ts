cd19035297f3367463d6214f95852d9648662fea
Wei <howard83124@gmail.com>
2023-09-13 20:04:49 +0800

    add github action to deploy to github page

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  server: {
-    host: '0.0.0.0'
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      host: '0.0.0.0'
+    },
+    base: mode === 'development' ? '' : '/vue-roulette'
   }
 })
